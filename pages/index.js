4289a9cd70f33336b7703fe81a3c70d03587cd83
francis omondi <francisomondi17@gmail.com>
2023-01-27 16:17:30 +0300

    clean up still

--- pages/index.js
+++ pages/index.js
@@ -1,7 +1,7 @@
 import Head from 'next/head'
 
 import { Inter } from '@next/font/google'
-import styles from '@/styles/Home.module.css'
+
 import Navbar from '@/components/Navbar'
 import Main from '@/components/Main'
 import About from '@/components/About'

463f8c9b4a791bb01687ed263207b769fbcbd135
francis omondi <francisomondi17@gmail.com>
2023-02-06 01:26:05 +0300

    polishing on projects

--- components/Projects.jsx
+++ components/Projects.jsx
@@ -7,27 +7,40 @@ import reactParallax from '../public/assets/portfolio/reactParallax.jpg'
 import reactSmooth from '../public/assets/portfolio/reactSmooth.jpg'
 import reactWeather from '../public/assets/portfolio/reactWeather.jpg'
 import navbar from '../public/assets/portfolio/navbar.jpg'
+import { MdOutlineKeyboardArrowRight } from 'react-icons/md'
 
 const Projects = () => {
 
 const projects =[
   {id:1,
-    src:arrayDestruct
+    src:arrayDestruct,
+    title: "Property Finder",
+    language: "React js"
   },
   {id:2,
-    src:installNode
+    src:installNode,
+    title: "Mawimbi",
+    language: "Node js"
   },
   {id:3,
-    src:reactParallax
+    src:reactParallax,
+    title: "Andela Geo App",
+    language: "Vanilla js"
   },
   {id:4,
-    src:reactSmooth
+    src:reactSmooth,
+    title: "StoryBook Api",
+    language: "Node js"
   },
   {id:5,
-    src:reactWeather
+    src:reactWeather,
+    title: "vidly Movies",
+    language: "Node js"
   },
   {id:6,
-    src:navbar
+    src:navbar,
+    title: "sakakeja",
+    language: "Laravel"
   },
 ]
 
@@ -39,18 +52,23 @@ const projects =[
             <div className='grid sm:grid-cols-2 md:grid-cols-3 gap-8'>
 
             {
-              projects.map(({id,src})=>(
+              projects.map(({id,src,title,language})=>(
                  <div key={id} className='relative flex items-center h-auto w-full shadow-xl
                 shadow-gray-500 rounded-xl p-4 group hover:bg-gradient-to-r
                   from-[#201f1f9e] to-[#8c8787]'>
 
                   <Image className='rounded-xl group-hover:opacity-10' src={src} alt='this is the alt'/>
                   <div className='hidden group-hover:block absolute top-[50%] left-[50%] translate-x-[-50%] translate-y-[-50%]'>
-                        <h3 className='text-2xl text-white -tracking-wider text-center' >Property finder</h3>
-                        <p className='pb-4 pt-2 text-white text-center'>React Js</p>
-                        <Link href='/'>
-                            <p className='text-center py-3 rounded-lg bg-white text-gray-800 font-bold text-lg cursor-pointer'>More Info</p>
-                        </Link>
+                        <h3 className='text-2xl text-white -tracking-wider text-center' >{title}</h3>
+                        <p className='pb-4 pt-2 text-white text-center'>{language}</p>
+                       <div>
+            <button className=' group w-fit px-6 py-3 my-2 flex items-center rounded-md cursor-pointer'>
+            More info
+              <span className='group-hover:rotate-90 duration-500 '>
+              <MdOutlineKeyboardArrowRight size={25} className='ml-3'/>
+              </span>
+            </button>
+          </div>
                   </div>
                 </div>
 
